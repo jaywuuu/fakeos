1631bcdd9056e74b65a9c8b1d7c47968ed8f2177
jaywuuu <jason.k.wu@gmail.com>
2021-10-07 10:11:11 -0400

    Add code to write in bootloader.

--- tools/make_boot.cpp
+++ tools/make_boot.cpp
@@ -11,12 +11,45 @@
 void printHelp() {
     std::cout << "make_boot version: " << MAKE_BOOT_VERSION_MAJOR << "."
               << MAKE_BOOT_VERSION_MINOR << std::endl;
-    std::cout << "Usage: make_boot DISK_IMG\n";
+    std::cout << "Usage: make_boot [OPTIONS] DISK_IMG\n";
     std::cout << "Writes 512 MB boot sector to DISK_IMG\n";
+    std::cout << "[OPTIONS]\n";
+    std::cout << "-b bootloader     Writes bootloader.\n";
 }
 
+struct Options {
+    bool writeBootloader;
+    std::string bootloaderPath;
+    std::string imgFilePath;
+};
+
+Options processCommandLineArgs(int argc, char* argv[]) {
+    Options cmdLineOptions = {};
+    cmdLineOptions.imgFilePath = "";
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-b") == 0) {
+            if (i+1 < argc) {
+                cmdLineOptions.writeBootloader = true;
+                cmdLineOptions.bootloaderPath = argv[++i];
+            }
+        } else if (strcmp(argv[i], "-h") == 0) {
+            printHelp();
+        } else {
+            cmdLineOptions.imgFilePath = argv[i];
+        }
+    }
+
+    return cmdLineOptions;
+}
+
+#pragma pack(1)
 struct MBR {
-    uint8_t filler[510];
+    uint8_t bootLoader[446];
+    uint32_t partitionEntry0[4];
+    uint32_t partitionEntry1[4];
+    uint32_t partitionEntry2[4];
+    uint32_t partitionEntry3[4];
     uint16_t magicNumber;
 };
 
@@ -25,9 +58,28 @@ void setupMBR(MBR& mbr) {
     mbr.magicNumber = BOOT_MAGIC_NUMBER;
 }
 
-void writeBoot(std::fstream& img) {
+void writeBoot(Options& opts, std::fstream& img) {
     MBR mbr = {0};
     setupMBR(mbr);
+
+    if (opts.writeBootloader) {
+        std::ifstream bootloader;
+        bootloader.open(opts.bootloaderPath, std::ios::in | std::ios::binary);
+        if (!bootloader.is_open())
+            throw std::runtime_error("Could not open bootloader file: " + opts.bootloaderPath);
+        
+        size_t maxBootloaderSize = sizeof(mbr.bootLoader);
+        bootloader.seekg(0, bootloader.end);
+        size_t bootloaderSize = bootloader.tellg();
+
+        if (bootloaderSize <= maxBootloaderSize) {
+            bootloader.seekg(0, bootloader.beg);
+            bootloader.read(reinterpret_cast<char*>(mbr.bootLoader), bootloaderSize);
+        }
+
+        bootloader.close();
+    }
+
     img.write(reinterpret_cast<const char*>(&mbr), sizeof(MBR));
 }
 
@@ -37,7 +89,14 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    std::string imgFilePath = argv[1];
+    Options cmdLineOptions = processCommandLineArgs(argc, argv);
+
+    if (cmdLineOptions.imgFilePath.length() == 0) {
+        printHelp();
+        return 0;
+    }
+
+    std::string imgFilePath = cmdLineOptions.imgFilePath;
 
     std::fstream imgFile;
     imgFile.open(imgFilePath, std::ios::in | std::ios::out | std::ios::binary);
@@ -45,7 +104,7 @@ int main(int argc, char* argv[]) {
     if (!imgFile.is_open())
         throw std::runtime_error("Could not open file: " + imgFilePath);
 
-    writeBoot(imgFile);
+    writeBoot(cmdLineOptions, imgFile);
 
     imgFile.flush();
     imgFile.close();
